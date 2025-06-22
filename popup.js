66aef0c9d3f098224eea85476fada6117b3a2484
netjeff <jeff@netjeff.com>
2025-06-22 14:15:50 -0600

    Remove button from popup that is no longer used.

--- popup.js
+++ popup.js
@@ -2,14 +2,11 @@
 window.addEventListener('DOMContentLoaded', async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const isYouTube = tab.url && tab.url.match(/^https?:\/\/(www\.|m\.|[a-z]{2}\.)?youtube\.com\//i);
-  const copyBtn = document.getElementById('copyBtn');
   const status = document.getElementById('status');
   if (!isYouTube) {
-    copyBtn.style.display = 'none';
     status.textContent = 'This extension only works on YouTube pages.';
     status.style.color = 'red';
   } else {
-    copyBtn.style.display = 'none';
     status.textContent = 'Copied!';
     status.style.color = 'green';
     chrome.scripting.executeScript({
